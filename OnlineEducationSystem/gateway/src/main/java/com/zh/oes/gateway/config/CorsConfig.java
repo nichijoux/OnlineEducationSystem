ef1b57491843d410257a9ae524464f3735f1857f
nichijoux <1647365387@qq.com>
2022-07-28 19:07:39 +0800

    api网关模块

---
+++ OnlineEducationSystem/gateway/src/main/java/com/zh/oes/gateway/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.zh.oes.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+// 跨域配置
+@Configuration
+@ComponentScan(basePackages = {"com.zh.oes"})
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
