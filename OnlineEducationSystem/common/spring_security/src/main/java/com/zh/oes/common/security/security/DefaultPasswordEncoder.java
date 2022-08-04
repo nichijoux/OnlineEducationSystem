c15fd9852a27fc7d288dcbd3867c39bc6a7009d9
nichijoux <1647365387@qq.com>
2022-08-04 21:40:07 +0800

    更换加密为BCRPT加密

--- OnlineEducationSystem/common/spring_security/src/main/java/com/zh/oes/common/security/security/DefaultPasswordEncoder.java
+++ OnlineEducationSystem/common/spring_security/src/main/java/com/zh/oes/common/security/security/DefaultPasswordEncoder.java
@@ -1,21 +1,29 @@
 package com.zh.oes.common.security.security;
 
-import com.zh.oes.common.utils.MD5Util;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 // 密码处理,MD5加密
 @Component
 public class DefaultPasswordEncoder implements PasswordEncoder {
+
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public DefaultPasswordEncoder() {
+        passwordEncoder = new BCryptPasswordEncoder();
+    }
+
     // 把未进行加密的密码进行MD5加密
     @Override
     public String encode(CharSequence rawPassword) {
-        return MD5Util.getMD5(rawPassword.toString());
+        System.out.println(rawPassword + "加密为" + passwordEncoder.encode(rawPassword));
+        return passwordEncoder.encode(rawPassword);
     }
 
     // 将未加密密码和数据库中的密码进行比对
     @Override
     public boolean matches(CharSequence rawPassword, String encodedPassword) {
-        return encodedPassword.equals(MD5Util.getMD5(rawPassword.toString()));
+        return passwordEncoder.matches(rawPassword, encodedPassword);
     }
 }
